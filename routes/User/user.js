e913401197ce0c5b30357c92530fc8a09e8819ef
Monia-L <monia.laoufi@gmail.com>
2019-06-05 17:02:11 +0200

    I just created the first GETs. The rest comes

--- routes/User/user.js
+++ routes/User/user.js
@@ -7,5 +7,9 @@ const router = express.Router();
 router.get("/", (req, res) => {
     res.send("I'm on GET '/user' ")
 })
+router.get("/:id", (req, res) => {
+    const id = req.params.id
+    res.send(`I'm on GET '/user/:id' - ${id}`)
+})
 
 module.exports = router;
