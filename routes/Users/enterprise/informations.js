8bd6fff5a1e41b5e6d9b072cbdae038b006a23d5
Arnaud Malanda <arnaud.malanda@gmail.com>
2019-06-12 17:28:31 +0200

    [Update][WIP] Basic CRUD for enterprise informations

--- routes/Users/enterprise/informations.js
+++ routes/Users/enterprise/informations.js
@@ -4,16 +4,29 @@ const connection = require('../../../helper/db')
 // Router
 const router = express.Router();
 
+// GETTING DATA FOR ENTERPRISE INFORMATIONS
 router.get("/", (req, res) => {
-    res.send("I'm on GET '/enterprise_info' ")
+    // Retrieving user enterprise id
+    const userId = req.id
+    const sql = "SELECT * FROM enterprise_informations WHERE user_id = ?"
+    connection.query(sql, userId, (err, result) => {
+        if (err)
+          throw res
+            .status(500)
+            .send("There was a problem finding the users.");
+        return res.status(200).send(result)
+    })
 })
 
-router.post("/", (req, res) => {
-    console.log("req BODY", req.body)
-    const sql = "INSERT INTO enterprise_informations" + 
-                "(n_siret, address, postcode, city, country, numbe_of_employees, picture, description, mail, phone) " + 
-                "VALUES (?,?,?,?,?,?,?,?,?,?)"
+// ADDS ENTERPRISE INFORMATIONS
+router.post('/', (req, res) => {
+    // Retrieving user enterprise id
+    const userId = req.id
+    console.log(req.id)
+    // SQL Request
+    const sql = "INSERT INTO enterprise_informations (user_id , n_siret, address, postcode, city, country, number_of_employees, picture, description, mail, phone) VALUES (?,?,?,?,?,?,?,?,?,?,?)"
     const values = [
+        userId,
         req.body.n_siret,
         req.body.address,
         req.body.postcode,
@@ -25,13 +38,46 @@ router.post("/", (req, res) => {
         req.body.mail,
         req.body.phone
     ]
-    connection.query(sql, values, (err, result) => {
-        if (err)
-          throw res
-            .status(500)
-            .send("There was a problem adding the information to the database.");
-        return res.status(200).send(result)
-    })
+    // connexion à la base de données, et insertion des driver_papers
+    connection.query(sql, values, (err, results) => {
+        if (err) {
+            // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+            console.log(err);
+            res.status(500).send("There was a problem adding enterprise informations to the database.");
+        } else {
+            console.log(results)
+            // Si tout s'est bien passé, on envoie un statut "ok".
+            res.status(200).send("Enterprise informations have been added")
+        }
+    });
 });
+
+// UPDATES ENTERPRISE INFORMATIONS
+router.put('/', (req, res) => {
+    // Retrieving user enterprise id
+    const userId = req.id
+    const formData = req.body
+    // SQL REQUEST
+    const sql = "UPDATE user SET ? WHERE user_id = ?";
+    connection.query(sql, [formData, userId], (err, result) => {
+        if (err) throw err;
+        return res.sendStatus(200).send(result.affectedRows);
+    });
+});
+
+// DELETES ENTERPRISE INFORMATIONS
+router.delete("/", (req, res) => {
+    // Retrieving user enterprise id
+    const userId = req.id
+    // SQL Request
+    const sql = "DELETE FROM enterprise_informations WHERE user_id = ?";
+    connection.query(sql, userId, (err, user) => {
+        if (err)
+          return res
+            .status(500)
+            .send("There was a problem deleting enterprise informations.");
+        res.status(200).send("Enterprise informations have been deleted");
+      });
+})
 
 module.exports = router;
