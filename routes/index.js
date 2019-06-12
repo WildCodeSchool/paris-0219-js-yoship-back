29fb7e6909e579071b5ac56d86186f502e4e3fbd
Agueda Perez <aguedapm@yahoo.com>
2019-06-12 16:29:17 +0200

    deleted unitilised User folder

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,4 @@
 const users = require("./Users/users")
 const driver_papers = require("./Users/Driver/driver_papers")
 // Routes export
-module.exports = { user, driver_papers };
+module.exports = { users, driver_papers };
