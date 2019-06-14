b04a367b4944a36a780b1b2285344229adb7b82a
Agueda Perez <aguedapm@yahoo.com>
2019-06-14 15:17:27 +0200

    Added cars.js routes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@
 const users = require("./Users/users")
 const cars = require("./Users/Drivers/Cars/cars")
 const car_documents = require("./Users/Drivers/Cars/car_documents")
-const driver_stats = require ("./Users/Drivers/driver_stats")
 const driver_papers = require("./Users/Drivers/driver_papers")
+const driver_stats = require ("./Users/Drivers/driver_stats")
 const enterpriseInfo = require("./Users/enterprise/informations")
 
 // Routes export
-module.exports = { users, driver_papers, driver_stats, enterpriseInfo, cars, car_documents };
+module.exports = { users, cars, car_documents, driver_papers, driver_stats, enterpriseInfo };
