a3a4b73f453cc114fa97d144abfe256abc415f2b
Arnaud Malanda <arnaud.malanda@gmail.com>
2019-06-24 18:17:59 +0200

    [Update] Setting up routes for auth controllers & config super pass

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // Getting all routes
 const routes = require("./routes/index")
@@ -11,6 +12,7 @@ const app = express();
 
 // Middlewares
 app.use(morgan("dev"));
+app.use(cors());
 
 // Body Parser configuration
 app.use(bodyParser.urlencoded({extended: false}))
@@ -18,13 +20,15 @@ app.use(bodyParser.json());
 
 // Sending to the right routes
 
+// Authentication
+app.use("/", routes.auth)
 // Users
 app.use("/users", routes.users)
 // Drivers
 app.use("/users/:id/driver_stats", routes.driver_stats)
 app.use("/users/:id/driver_papers", routes.driver_papers)
 // Cars
-app.use("/users/:id/cars", function(req, res, next) {
+app.use("/users/:id/cars", (req, res, next) => {
     // Passes the users id to the next route
     req.id = req.params.id; 
     next()
@@ -32,7 +36,7 @@ app.use("/users/:id/cars", function(req, res, next) {
 // Car documents
 app.use("/users/:id/cars/:carId/car_documents", routes.car_documents)
 // Enterprise
-app.use('/users/:id/enterprise_info', function(req, res, next) {
+app.use('/users/:id/enterprise_info', (req, res, next) => {
     // The enterprise_info route will use the user's id from the users route in parameters to retrieve the corresponding dataset in the database
     // Here we are passing the user's id to the next route
     req.id = req.params.id; 
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const car_documents = require("./Users/Drivers/Cars/car_documents")
 const driver_papers = require("./Users/Drivers/driver_papers")
 const driver_stats = require ("./Users/Drivers/driver_stats")
 const enterpriseInfo = require("./Users/enterprise/informations")
+const auth = require("./auth/authController")
 
 // Routes export
-module.exports = { users, cars, car_documents, driver_papers, driver_stats, enterpriseInfo };
+module.exports = { users, cars, car_documents, driver_papers, driver_stats, enterpriseInfo, auth };
