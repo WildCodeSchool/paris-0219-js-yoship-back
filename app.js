749746f919eea0e3dbf61bf3cdb96dc28a5d7dda
Arnaud Malanda <arnaud.malanda@gmail.com>
2019-06-25 14:47:39 +0200

    [Update] Permit done on user route & changed req.id to req.userId in app

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use("/users/:id/driver_papers", routes.driver_papers)
 // Cars
 app.use("/users/:id/cars", (req, res, next) => {
     // Passes the users id to the next route
-    req.id = req.params.id; 
+    req.userId = req.params.id; 
     next()
 },routes.cars)
 // Car documents
@@ -39,7 +39,7 @@ app.use("/users/:id/cars/:carId/car_documents", routes.car_documents)
 app.use('/users/:id/enterprise_info', (req, res, next) => {
     // The enterprise_info route will use the user's id from the users route in parameters to retrieve the corresponding dataset in the database
     // Here we are passing the user's id to the next route
-    req.id = req.params.id; 
+    req.userId = req.params.id; 
     next()
 }, routes.enterpriseInfo);
 
--- routes/Users/users.js
+++ routes/Users/users.js
@@ -17,13 +17,13 @@ router.get("/", VerifyToken, permit('admin'), (req, res) => {
     })
 })
 
-router.post("/", (req, res) => {
+router.post("/", VerifyToken, permit('admin'), (req, res) => {
   // récupération des données envoyées
   const formData = req.body;
   console.log('console.log formData: ', formData);
   // connexion à la base de données, et insertion de l'employé
   connection.query('INSERT INTO user SET ?', formData, (err, results) => {
-
+  
     if (err) {
       // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
       console.log(err);
@@ -36,7 +36,7 @@ router.post("/", (req, res) => {
   });
 })
 
-router.put("/", (req, res) => {
+router.put("/", VerifyToken, permit('admin'), (req, res) => {
   const formData = req.body;
   connection.query('UPDATE user SET ?', [formData], err => {
     if (err) {
@@ -57,38 +57,27 @@ router.delete('/', VerifyToken, permit('admin'), (req, res) =>{
       console.log(err);
       res.status(500).send("Erreur lors de la suppression des users");
     } else {
-
       // Si tout s'est bien passé, on envoie un statut "ok".
       res.sendStatus(200);
     }
   });
 })
 
-
-router.get("/:id", VerifyToken, (req, res) => {
-    const idUsers = Number(req.params.id)
-    // res.send(`I'm on GET '/user/:id' - ${id}`)
-    console.log(req.id)
-    console.log(req.params.id)
-    connection.query(`SELECT * FROM user WHERE id = ?`, [idUsers], (err, results) => {
+router.get("/:id", VerifyToken, permit('admin', 'driver'), (req, res) => {
+    const idUser = req.params.id
+    connection.query(`SELECT * FROM user WHERE id = ?`, [idUser], (err, results) => {
         if (err)
           // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
           throw res.status(500).send("Erreur lors de la récupération des users");
-        if (idUsers === req.id || req.role.name === "admin") {
-            // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
-            return res.status(200).json(results);
-        } else {
-            return res.status(403).json({
-                message: "User is not authorized to read"
-            });
-        }  
+        // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
+        return res.status(200).json(results); 
     })
 })
 
-router.put("/:id", (req, res) => {
-  const idUsers = req.params.id;
+router.put("/:id", permit('admin', 'driver'), (req, res) => {
+  const idUser = req.params.id;
   const formData = req.body;
-  connection.query('UPDATE user SET ? WHERE id = ?', [formData, idUsers], err => {
+  connection.query('UPDATE user SET ? WHERE id = ?', [formData, idUser], err => {
     if (err) {
     // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
       console.log(err);
